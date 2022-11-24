5637c980e98aa75a900ef6a23be73e85cd552a0b
DTsigie22 <dtsigie@terpmail.umd.edu>
2022-11-23 23:14:37 -0500

    format fix

--- client/script.js
+++ client/script.js
@@ -11,15 +11,15 @@ fetch('https://api-nba-v1.p.rapidapi.com/teams', options)
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
 
-    const options1 = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
-            'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
-        }
-    };
+const options1 = {
+    method: 'GET',
+    headers: {
+         'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+        'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
+    }
+ };
     
-    fetch('https://api-nba-v1.p.rapidapi.com/teams/statistics?id=1&season=2020', options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
+ fetch('https://api-nba-v1.p.rapidapi.com/teams/statistics?id=1&season=2020', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
